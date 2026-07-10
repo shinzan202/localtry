9890242c9d21499ed962d7b234e9c119a88c90a1
shinzan <shinzan@foxmail.com>
2026-07-10 11:05:12 +0800

    add cmake

---
+++ build/CMakeFiles/localtry.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/localtry.dir/main.cpp.obj"
+  "CMakeFiles/localtry.dir/main.cpp.obj.d"
+  "liblocaltry.dll.a"
+  "localtry.exe"
+  "localtry.exe.manifest"
+  "localtry.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/localtry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/localtry.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for localtry.
+# This may be replaced when dependencies are built.
